4f5d66d6eb024c0cfed04d8017b44263de68f0b4
unknown <asmak9060@gamil.com>
2021-06-08 21:25:35 +0500

    1st commit

---
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.BtnNoti);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message="this is a Notification";
+                NotificationCompat.Builder builder=new NotificationCompat.Builder(MainActivity.this);
+            }
+        });
+    }
+}
